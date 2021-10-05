4ae65f1eaba9cdfbb98fba62c4315ed4974fc1dd
Yonry Zhu <yonry.github@gmail.com>
2021-10-05 10:56:03 -0400

    Initial commit

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restx import Resource, Api
+
+
+app = Flask(__name__)
+
+api = Api(app)
+
+
+app.config.from_object("src.config.DevelopmentConfig")
+
+
+class Ping(Resource):
+    def get(self):
+        return {"status": "success", "message": "pong!"}
+
+
+api.add_resource(Ping, "/ping")
