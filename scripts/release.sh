b1e4cb1d6a62523c83869883ad6bae621a4be811
Yonry Zhu <yonry.github@gmail.com>
2021-10-21 17:40:35 -0400

    I am the big dumb

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,5 +8,5 @@ PAYLOAD='{"updates": [{"type": "web", "docker_image": "'"$IMAGE_ID"'"}]}'
 curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation \
 	-d "$PAYLOAD" \
 	-H "Content-Type: application/json" \
-	-H "Accept: application/vnd.heroku+json; version=3.docker-releases"
+	-H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
 	-H "Authorization: Bearer $HEROKU_AUTH_TOKEN"
