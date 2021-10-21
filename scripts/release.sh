06d14949c3e7c9067ecdcd9c0fe1ab6912811d04
Yonry Zhu <yonry.github@gmail.com>
2021-10-21 17:25:24 -0400

    Actually fixed release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-IMAGE_ID=$(docker inspect ${HEROKU_IMAGE} --format={{.Id}})
+IMAGE_ID=$(docker inspect $HEROKU_IMAGE --format={{.Id}})
 PAYLOAD='{"updates": [{"type": "web", "docker_image": "'"$IMAGE_ID"'"}]}'
 
 curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation \
